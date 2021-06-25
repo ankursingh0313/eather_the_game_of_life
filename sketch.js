49fa6f73cb63de3f8f43f7c0337d84df323bd004
ankur256 <ankursingh0313@gmail.com>
2021-06-25 20:51:23 +0530

    compleated

--- sketch.js
+++ sketch.js
@@ -1,47 +1,50 @@
 let grid;
-const height = 800;
-const width = 1200;
-const resolution = 10;
-const rows = height / resolution;
-const cols = width / resolution;
-const frame_rate = 1;
-const border_width = .05;
-const maximum_age = 3;
+const resolution    = 10;
+const window_height = parseInt(window.innerHeight * 70 / 100);
+const window_width  = parseInt(window.innerWidth * 80 / 100);
+const height        = window_height - (window_height % resolution);
+const width         = window_width - (window_width % resolution);
+const rows          = height / resolution;
+const cols          = width / resolution;
+const frame_rate    = 1;
+const border_width  = .05;
 
-let simulation_started = false;
-let ultimate_trooth = false;
-let generation = 0;
-let total_live = 0;
-let total_dead = 0;
-let total_born = 0;
+let simulation_started  = false;
+let ultimate_trooth     = false;
+let can_loop            = false;
+let generation          = 0;
+let total_live          = 0;
+let total_dead          = 0;
+let total_born          = 0;
+let maximum_age         = 1;
 
 // analysys
-const scorecard_width = 180;
-const scorecard_height = 80;
-const percentage_chart_width = 300;
-const percentage_chart_height = 20;
+const scorecard_width           = 180;
+const scorecard_height          = 80;
+const percentage_chart_width    = 300;
+const percentage_chart_height   = 20;
 // color
-const just_died_color = 'rgba(255, 10, 10, 1)';
-const just_born_color = 'rgba(200, 255, 0, 1)';
-const aging_color = 'rgba(0, 158, 0, 1)';
-const alive_color = 'rgba(0, 0, 255, 1)';
-const old_color = 'rgba(200, 0, 255,1)';
-const beyond_color = 'rgba(0, 0, 0, 1)';
+const just_died_color           = 'rgba(255, 10, 10, 1)';
+const just_born_color           = 'rgba(200, 255, 0, 1)';
+const aging_color               = 'rgba(0, 158, 0, 1)';
+const alive_color               = 'rgba(0, 0, 255, 1)';
+const old_color                 = 'rgba(200, 0, 255,1)';
+const beyond_color              = 'rgba(0, 0, 0, 1)';
 
-const scoreboard_color = 'rgba(0,0,200, .3)';
-const scoreboard_text_color = 'rgba(0,0,0,1)';
+const scoreboard_color          = 'rgba(0,0,200, .3)';
+const scoreboard_text_color     = 'rgba(0,0,0,1)';
 
-const percentage_chart_alive_color = 'rgba(0, 150, 0, .5)';
-const percentage_chart_dead_color = 'rgba(255, 100, 0, .5)';
-const percentage_chart_bg_color = 'rgba(100, 100, 100, .5)';
+const percentage_chart_alive_color  = 'rgba(0, 150, 0, .5)';
+const percentage_chart_dead_color   = 'rgba(255, 100, 0, .5)';
+const percentage_chart_bg_color     = 'rgba(100, 100, 100, .5)';
 class Creature {
     constructor(x, y, state, life_span) {
-        this.x = x;
-        this.y = y;
-        this.state = state;
-        this.life_span = life_span;
-        this.just_died = false;
-        this.just_born = false;
+        this.x          = x;
+        this.y          = y;
+        this.state      = state;
+        this.life_span  = life_span;
+        this.just_died  = false;
+        this.just_born  = false;
         this.neighbours = [
             [x-1, y],
             [x-1, y-1],
@@ -54,8 +57,8 @@ class Creature {
         ];
     }
     updateState() {
-        let sum = 0;
-        let neighbours = this.neighbours;
+        let sum         = 0;
+        let neighbours  = this.neighbours;
         for (let i = 0; i < neighbours.length; i++) {
             if (this.x == 0 && this.y == 0 && ((i >= 0 && i < 4) || (i > 6 && i < 8))) {
                 sum += 0;
@@ -77,30 +80,27 @@ class Creature {
                 sum += grid[neighbours[i][0]][neighbours[i][1]].state
             }
         }
-        if (this.x == 1 && this.y == 1) {
-            // optional
-        }
-        if (ultimate_trooth && this.state == 1 && this.life_span > maximum_age) {
-            this.state = 0;
-            this.just_died = true;
-            this.just_born = false;
-            this.life_span = 0;
+        if (ultimate_trooth && this.state == 1 && this.life_span >= maximum_age) {
+            this.state      = 0;
+            this.just_died  = true;
+            this.just_born  = false;
+            this.life_span  = 0;
         } else if (sum > 2  && this.state == 0) {
-            this.state = 1;
-            this.just_died = false;
-            this.just_born = true;
-            this.life_span = 0;
+            this.state      = 1;
+            this.just_died  = false;
+            this.just_born  = true;
+            this.life_span  = 0;
         } else if ((sum < 2 || sum > 3) && this.state == 1) {
-            this.state = 0;
-            this.just_died = true;
-            this.just_born = false;
-            this.life_span = 0;
+            this.state      = 0;
+            this.just_died  = true;
+            this.just_born  = false;
+            this.life_span  = 0;
         } else if (sum > 1 && sum < 4 && this.state == 1) {
-            this.just_born = false;
-            this.life_span += 1;
+            this.just_born  = false;
+            this.life_span  += 1;
         } else if (sum < 2 || sum > 3 && this.state == 0) {
-            this.just_died = false;
-            this.life_span = 0;
+            this.just_died  = false;
+            this.life_span  = 0;
         } else {
             if (this.just_died) {
                 this.just_died = false;
@@ -113,18 +113,18 @@ class Creature {
         }
     }
     changeState() {
-        this.state = this.state == 0?1:0;
-        this.life_span = this.state == 0?0:0;
-        this.just_born = this.state == 0?false:true;
-        total_live += this.state == 0?-1:1;
-        total_born = total_live;
+        this.state      = this.state == 0?1:0;
+        this.life_span  = this.state == 0?0:0;
+        this.just_born  = this.state == 0?false:true;
+        total_live      += this.state == 0?-1:1;
+        total_born      = total_live;
     }
 }
 function setup() {
-    let gameboard = createCanvas(width, height);
+    let gameboard   = createCanvas(width, height);
     gameboard.parent('game_board');
     background(0);
-    grid = createGrid();
+    grid            = createGrid();
     frameRate(frame_rate);
 }
 function draw() {
@@ -219,11 +219,11 @@ function createColorinfo() {
     text("The ultimate Age", 30, 122);
 }
 function createPercentageChart() {
-    let total = total_live + total_dead - total_born;
+    let total           = total_live + total_dead - total_born;
     let born_percentage = (total_born/total)*100;
     let dead_percentage = (total_dead/total)*100;
-    let born_value = percentage_chart_width * (born_percentage/100);
-    let dead_value = percentage_chart_width * (dead_percentage/100);
+    let born_value      = percentage_chart_width * (born_percentage/100);
+    let dead_value      = percentage_chart_width * (dead_percentage/100);
     fill(percentage_chart_bg_color);
     rect(resolution * cols - percentage_chart_width, resolution * rows - percentage_chart_height * 2, percentage_chart_width, percentage_chart_height * 2);
     fill(percentage_chart_alive_color);
@@ -233,19 +233,20 @@ function createPercentageChart() {
 
 }
 function startSimulation(e) {
-    console.log("hi", e)
-    if (!simulation_started) {
-        simulation_started = !simulation_started;
-    }
+    simulation_started  = !simulation_started;
+    can_loop            = !can_loop;
     if (simulation_started) {
         e.innerText = "Stop Simulation";
+        e.setAttribute("class", "btn btn-danger")
     } else {
         e.innerText = "Start Simulation";
+        e.setAttribute("class", "btn btn-primary")
     }
+    console.log(simulation_started);
 }
 
 function runSimulation() {
-    let temp_grid = copyGrid(grid);
+    let temp_grid = copyGrid(grid, true);
     for (let i = 0; i<cols; i++) {
         for (let j = 0; j<rows; j++) {
             temp_grid[i][j].updateState();
@@ -254,26 +255,28 @@ function runSimulation() {
     let t = grid;
     grid = copyGrid(temp_grid);
 }
-function copyGrid(c_grid) {
-    let all_live = 0;
-    let all_just_died = 0;
-    let all_just_born = 0;
-    let new_grid = new Array();
+function copyGrid(c_grid, calculate=false) {
+    let all_live        = 0;
+    let all_just_died   = 0;
+    let all_just_born   = 0;
+    let new_grid        = new Array();
     for (let i = 0; i<=cols; i++) {
         new_grid[i] = new Array();
         for (let j = 0; j<=rows; j++) {
-            all_live += c_grid[i][j].state==1?1:0;
-            all_just_born += c_grid[i][j].just_born?1:0;
-            all_just_died += c_grid[i][j].just_died?1:0;
-            let obj = new Creature(i, j, c_grid[i][j].state, c_grid[i][j].life_span);
-            obj.just_died = c_grid[i][j].just_died;
-            obj.just_born = c_grid[i][j].just_born;
-            new_grid[i][j] = obj;
+            all_live        += c_grid[i][j].state==1?1:0;
+            all_just_born   += c_grid[i][j].just_born?1:0;
+            all_just_died   += c_grid[i][j].just_died?1:0;
+            let obj         = new Creature(i, j, c_grid[i][j].state, c_grid[i][j].life_span);
+            obj.just_died   = c_grid[i][j].just_died;
+            obj.just_born   = c_grid[i][j].just_born;
+            new_grid[i][j]  = obj;
         }
     }
-    total_live = all_live;
-    total_dead = all_just_died;
-    total_born = all_just_born;
+    if (calculate) {
+        total_live = all_live;
+        total_dead = all_just_died;
+        total_born = all_just_born;
+    }
     return new_grid;
 }
 function createGrid() {
@@ -287,12 +290,11 @@ function createGrid() {
     return grid;
 }
 function mouseClicked() {
-    if (mouseX < 0 || mouseX*resolution > width || mouseY < 0 || mouseY > height) {
-        
+    if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
     } else {
-        let x = parseInt(mouseX / resolution);
-        let y = parseInt(mouseY / resolution);
-        let node = grid[x][y];
+        let x   = parseInt(mouseX / resolution);
+        let y   = parseInt(mouseY / resolution);
+        let node= grid[x][y];
         if (node.state == 0) {
             fill(just_born_color);
             stroke(0);
@@ -303,5 +305,5 @@ function mouseClicked() {
         rect(mouseX - (mouseX % resolution), mouseY - (mouseY % resolution), resolution, resolution);
         node.changeState();
     }
-    return false;
+    // return false;
 }
